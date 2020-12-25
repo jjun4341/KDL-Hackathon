48be427581172e66fea85e719d1a9e78a043530d
peanut <42130391+jjun4341@users.noreply.github.com>
2020-12-25 10:58:32 +0900

    [add / 20201225 / index.py 파일 업로드 ]

---
+++ index.py
@@ -0,0 +1,24 @@
+import discord
+import asyncio
+from discord.ext import commands
+from discord.ext.commands import bot
+import koreanbots
+import os
+
+bot = commands.Bot(command_prefix='+', help_command=None)
+
+startup_extensions = ['cogs.Ticket_tool.py', 'cogs.Developer']
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('불러오기에 실패 하였습니다. 에러 파일 : {}\n에러 내용 : {}'.format(extension,exc))
+
+@bot.event
+async def on_ready():
+    print('Client is ready')
+
+bot.run('Nzc1MzI4NTI5ODg1MDM2NTk1.X6ku9g.4ukIjK8yg7jOGIkvCjM29fdd6Q0')
